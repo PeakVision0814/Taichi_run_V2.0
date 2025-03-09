0e6dc6a85be551f5befefbe451aa2616d55b5b46
PeakVision0814 <Perished_hgp@qq.com>
2025-03-09 14:41:00 +0800

    更新版本信息

--- code/heart_rate_monitor.py
+++ code/heart_rate_monitor.py
@@ -6,8 +6,7 @@ This module provides a class to simulate and monitor heart rate based on elapsed
 Author: Gaopeng Huang; Hui Guo
 Email: perished_hgp@163.com; gh1848026781@163.com
 Date Created: 2025-03-06
-Last Modified: 2025-03-06
-Version: 1.0.0
+Last Modified: 2025-03-09
 Copyright (c) 2025 PeakVision
 All rights reserved.
 This software is released under the GNU GENERAL PUBLIC LICENSE, see LICENSE for more information.
--- code/treadmill_controller.py
+++ code/treadmill_controller.py
@@ -6,8 +6,7 @@ This module provides a treadmill control system with heart rate monitoring and a
 Author: Gaopeng Huang; Hui Guo
 Email: perished_hgp@163.com; gh1848026781@163.com
 Date Created: 2025-03-06
-Last Modified: 2025-03-06
-Version: 2.1.0
+Last Modified: 2025-03-09
 Copyright (c) 2025 PeakVision Technologies
 All rights reserved.
 This software is released under the GNU GENERAL PUBLIC LICENSE, see LICENSE for more information.
